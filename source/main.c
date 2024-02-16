8694cc05e1dfd16916deb77b8c79fbc1ddbc5d7a
jens1608 <47902940+jens1608@users.noreply.github.com>
2024-02-16 22:36:41 +0100

    Update main.c
    Trying to disable PCM while waiting for data

--- source/main.c
+++ source/main.c
@@ -526,11 +526,15 @@ int main(int argc, char* argv[])
       // Zero fill the sample buffers for silence
       raspdif_fill_buffers(buffer_index, &block, arguments.format, arguments.sample_rate, arguments.keep_alive);
 
+      bcm283x_pcm_enable(false, false);
+
       // Wait for file to be readable
       struct pollfd poll_list;
       poll_list.fd = fileno(file);
       poll_list.events = POLLIN;
       poll(&poll_list, 1, -1);
+
+      bcm283x_pcm_enable(true, false);
 
       // Resume read loop
       LOGD(TAG, "Data available.");
